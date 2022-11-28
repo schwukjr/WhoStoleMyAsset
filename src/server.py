bd357539037412dadc6555ecc297ffd9fbd0bb4d
TristanMurphy2607 <tristan.murphy@motorolasolutions.com>
2022-11-28 18:14:04 +0000

    Added HTTP responses for adding assets and checking assets in/out.

--- src/server.py
+++ src/server.py
@@ -32,6 +32,45 @@ def newconfig():
         cursor.execute('INSERT INTO Configurations VALUES (NULL, ?, (SELECT ProjectID FROM Projects WHERE ProjectName = ?))', params)
     return '', 200
 
+@app.route("/newasset", methods=['POST']) #TODO: Add check to ensure no duplicates are allowed in single config.
+def newasset():
+    seconds_since_epoch = datetime.datetime.now().timestamp()
+    assetName = request.args.get('assetname', '')
+    configName = request.args.get('configname', '')
+    projectName = request.args.get('projectname', '')
+    connection = create_connection('db.sqlite')
+    with connection:
+        cursor = connection.cursor()
+        params = (assetName, configName, projectName, seconds_since_epoch)
+        cursor.execute('INSERT INTO Assets VALUES (NULL, ?, (SELECT ConfigID FROM Configurations WHERE ConfigName = ? AND ProjectID = (SELECT ProjectID FROM Projects WHERE ProjectName = ?)), 0, NULL, ?)', params)
+    return '', 200
+
+@app.route("/takeorreturnasset", methods=['POST'])
+def takeorreturnasset():
+    assetName = request.args.get('assetname', '')
+    configName = request.args.get('configname', '')
+    projectName = request.args.get('projectname', '')
+    takenBy = request.args.get('takenby', '')
+    
+    connection = create_connection('db.sqlite')
+    with connection:
+        cursor = connection.cursor()
+        params = (assetName, configName, projectName)
+        cursor.execute('SELECT Taken FROM Assets WHERE (assetName = ? AND configID = (SELECT ConfigID FROM Configurations WHERE ConfigName = ? AND ProjectID = (SELECT ProjectID FROM Projects WHERE ProjectName = ?)))', params)
+        rows = cursor.fetchall()
+
+        seconds_since_epoch = datetime.datetime.now().timestamp()
+        for row in rows:
+            if row[0] == 0:
+                params = (1, takenBy, seconds_since_epoch, assetName, configName, projectName)   
+            else:
+                params = (0, 'NULL', seconds_since_epoch, assetName, configName, projectName)   
+
+        cursor.execute('UPDATE Assets SET Taken = ?, TakenBy = ?, LastUpdated = ? WHERE (assetName = ? AND configID = (SELECT ConfigID FROM Configurations WHERE ConfigName = ? AND ProjectID = (SELECT ProjectID FROM Projects WHERE ProjectName = ?)))', params)
+
+
+    return '', 200
+
 def create_connection(path):
     connection = None
     try:
