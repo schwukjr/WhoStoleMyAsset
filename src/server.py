7db75a11db3b0a55cbc273cc7c2bdedbcae2608f
TristanMurphy2607 <tristan.murphy@motorolasolutions.com>
2022-12-09 14:56:45 +0000

    Added auto-filling of forms based on current project/config.

--- src/server.py
+++ src/server.py
@@ -9,15 +9,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    html = render_template('index.html')
+    html += renderProjectSelect()
+    return html
 
+def renderProjectSelect():
     connection = create_connection('db.sqlite')
     with connection:
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM Projects')
         projects = cursor.fetchall()
 
-        html = render_template('index.html')
-        html += f"""<form action="/getprojectdashboard" method="get">
+        html = f"""<form action="/getprojectdashboard" method="get">
                         <label for="projectName">Project:</label>
                         <select name="projectName" id="projectName" style="text-transform:uppercase">"""
         for project in projects:
@@ -28,82 +31,183 @@ def index():
         html += f"</form>"
     return html
 
-@app.route("/getprojectdashboard", methods=['GET'])
-def getProject(projectNameParam=""):
+def renderConfigButton(projectName):
     connection = create_connection('db.sqlite')
     with connection:
         cursor = connection.cursor()
-
-        if(projectNameParam == ""):
-            projectName = request.args.get('projectName', '')
-        else:
-            projectName = projectNameParam
-
         params = (projectName,)
         cursor.execute('SELECT * FROM Projects WHERE ProjectName = ?', params)
         projects = cursor.fetchall()
-
-        html = render_template('dashboard.html')
-
+        
+        
+        
         for project in projects:
-            ProjectID = project[0]
             ProjectName = project[1]
-            html += f"<h2 style=\"text-transform:uppercase\">{ProjectName}<h2>"
 
-            params = (ProjectID,)
-            cursor.execute(
-                'SELECT * FROM Configurations WHERE ProjectID = ?', params)
-            configs = cursor.fetchall()
-            for config in configs:
-                ConfigID = config[0]
-                ConfigName = config[1]
-                html += f"<h3 style=\"text-transform:capitalize\">{ConfigName}</h3>"
-
-                params = (ConfigID,)
-                cursor.execute(
-                    'SELECT * FROM Assets WHERE ConfigID = ? ORDER BY Taken DESC, LastUpdated DESC', params)
-                assets = cursor.fetchall()
-                html += "<div class=\"card-columns\" style=\"margin: 20px\">"
-                for asset in assets:
-                    AssetID = asset[0]
-                    AssetName = asset[1]
-                    Taken = asset[3]
-                    TakenBool = (Taken == 1)
-                    if TakenBool:
-                        colour = "solid red"
-                        buttonText = "Check In"
-                    else:
-                        colour = "solid green"
-                        buttonText = "Check Out"
-
-                    TakenBy = asset[4]
-                    LastUpdated = asset[5]
-                    lastUpdatedFormatted = time.strftime(
-                        '%Y-%m-%d %H:%M:%S', time.localtime(LastUpdated))
-
-                    html += f"""
-                    <div class="card text-center">
-                        <div class="card-header"><h3>{AssetName}</h3></div>
-                        <div class="card-body">
-                            <div class="card-text">
-                                Checked Out? {TakenBool}<br/>
-                                Checked out by: {TakenBy}<br/>
-                                Last Updated: {lastUpdatedFormatted}<br/>
+            html = f"""
+            <div id="newConfigForm" class="modal fade">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title">Add Config</h1>
+                    </div>
+                    <div class="modal-body">
+                        <form role="form" method="POST" action="/newconfig">
+                        <div class="form-group">
+                            <label class="control-label">Config Name</label>
+                            <div>
+                            <input type="text" class="form-control input-lg" name="configName" value="" />
                             </div>
                         </div>
-                        <div class="card-footer">
-                            <form action="/takeorreturnasset" method="post">
-                                <input type="text" name= "assetname" value="{AssetName}" hidden/>
-                                <input type="text" name= "configname" value="{ConfigName}" hidden/>
-                                <input type="text" name= "projectname" value="{ProjectName}" hidden/>
-                                <input type="text" name= "takenby" value="test" hidden/>
-                                <input type="submit" style="padding: 10px; border: 2px {colour}; text-align: center" value="{buttonText}" />
-                            </form>
+                        <div class="form-group">
+                            <label class="control-label">Project Name</label>
+                            <div>
+                            <input type="text" class="form-control input-lg" name="projectName" value="{ProjectName}" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div>
+                            <button type="submit" class="btn btn-success">Submit</button>
+                            </div>
+                        </div>
+                        </form>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#newConfigForm">Add Config</button>"""
+    return html
+
+def renderLoginButton(loginButtonCount, configName, projectName):
+    html = f"""
+        <div id="newLoginForm{loginButtonCount}" class="modal fade">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title">Add Login</h1>
+                </div>
+                <div class="modal-body">
+                    <form role="form" method="POST" action="/newlogin">
+                    <div class="form-group">
+                        <label class="control-label">Login Name</label>
+                        <div>
+                        <input type="text" class="form-control input-lg" name="loginName" value="" />
                         </div>
                     </div>
-                    """
-                html += "</div>"
+                    <div class="form-group">
+                        <label class="control-label">Config Name</label>
+                        <div>
+                        <input type="text" class="form-control input-lg" name="configName" value="{configName}" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Project Name</label>
+                        <div>
+                        <input type="text" class="form-control input-lg" name="projectName" value="{projectName}" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div>
+                        <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#newLoginForm{loginButtonCount}">Add Login</button>"""
     return html
+
+def renderContent(projectName):
+    connection = create_connection('db.sqlite')
+    with connection:
+        cursor = connection.cursor()
+        params = (projectName,)
+        cursor.execute('SELECT * FROM Projects WHERE ProjectName = ?', params)
+        projects = cursor.fetchall()
+        
+        i = 0
+        for project in projects:
+                ProjectID = project[0]
+                ProjectName = project[1]
+                html = f"<h2 style=\"text-transform:uppercase\">Project: {ProjectName}<h2>"
+                html += renderConfigButton(projectName)
+
+                params = (ProjectID,)
+                cursor.execute(
+                    'SELECT * FROM Configurations WHERE ProjectID = ?', params)
+                configs = cursor.fetchall()
+                for config in configs:
+                    ConfigID = config[0]
+                    ConfigName = config[1]
+                    html += f"<h3 style=\"text-transform:capitalize\">Config: {ConfigName}</h3>"
+                    print(ConfigName)
+                    html += renderLoginButton(i, ConfigName, ProjectName)
+                    i += 1
+
+                    params = (ConfigID,)
+                    cursor.execute(
+                        'SELECT * FROM Assets WHERE ConfigID = ? ORDER BY Taken DESC, LastUpdated DESC', params)
+                    assets = cursor.fetchall()
+                    html += "<div class=\"card-columns\" style=\"margin: 20px\">"
+                    for asset in assets:
+                        AssetID = asset[0]
+                        AssetName = asset[1]
+                        Taken = asset[3]
+                        TakenBool = (Taken == 1)
+                        if TakenBool:
+                            colour = "solid red"
+                            buttonText = "Check In"
+                        else:
+                            colour = "solid green"
+                            buttonText = "Check Out"
+
+                        TakenBy = asset[4]
+                        LastUpdated = asset[5]
+                        lastUpdatedFormatted = time.strftime(
+                            '%Y-%m-%d %H:%M:%S', time.localtime(LastUpdated))
+
+                        html += f"""
+                        <div class="card text-center">
+                            <div class="card-header"><h3>{AssetName}</h3></div>
+                            <div class="card-body">
+                                <div class="card-text">
+                                    Checked Out? {TakenBool}<br/>
+                                    Checked out by: {TakenBy}<br/>
+                                    Last Updated: {lastUpdatedFormatted}<br/>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <form action="/takeorreturnasset" method="post">
+                                    <input type="text" name= "assetname" value="{AssetName}" hidden/>
+                                    <input type="text" name= "configname" value="{ConfigName}" hidden/>
+                                    <input type="text" name= "projectname" value="{ProjectName}" hidden/>
+                                    <input type="text" name= "takenby" value="test" hidden/>
+                                    <input type="submit" style="padding: 10px; border: 2px {colour}; text-align: center" value="{buttonText}" />
+                                </form>
+                            </div>
+                        </div>
+                        """
+                    html += "</div>"
+    return html
+
+@app.route("/getprojectdashboard", methods=['GET'])
+def getProject(projectNameParam=""):
+    if(projectNameParam == ""):
+        projectName = request.args.get('projectName', '')
+    else:
+        projectName = projectNameParam
+
+    html = render_template('dashboard.html')
+    html += renderProjectSelect()
+    html += renderContent(projectName)
+        
+    return html
+
+
 
 
 @app.route("/newproject", methods=['POST'])
